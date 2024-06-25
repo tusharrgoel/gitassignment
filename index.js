0e2be3631d9026ffcc6462dae300f391e8594088
tusharrgoel <tushar.goel@practo.com>
2024-06-25 21:30:12 +0530

    feat:leapyear added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-
 function getAge(birthYear) {
-    const age =  2024 - birthYear;
-    console.log(age)
+  const age = 2024 - birthYear;
+  console.log(age);
 }
-function isValidVoter(birthYear){
-    const age = 2024-birthYear;
-    console.log(age > 21 ? "Eligible to vote" : "Not eligible to vote");
+function isValidVoter(birthYear) {
+  const age = 2024 - birthYear;
+  console.log(age > 21 ? "Eligible to vote" : "Not eligible to vote");
+}
+function isLeapYear(year) {
+  console.log(year % 4 === '0' ? "Leap Year" : "Not a leap year");
 }
 getAge(2002);
 isValidVoter(2000);
+isLeapYear(2001);

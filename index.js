ea8a6f0a3936ac986ec26c9d2cdedf2492c438cb
tusharrgoel <tushar.goel@practo.com>
2024-06-25 21:26:55 +0530

    getAge and isValidVoter added

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function getAge(birthYear) {
+    const age =  2024 - birthYear;
+    console.log(age)
+}
+function isValidVoter(birthYear){
+    const age = 2024-birthYear;
+    console.log(age > 21 ? "Eligible to vote" : "Not eligible to vote");
+}
+getAge(2002);
+isValidVoter(2000);

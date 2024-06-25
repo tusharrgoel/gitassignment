7f3f66d4a0b99877844b1e148fb5168adbe9bf9f
tusharrgoel <tushar.goel@practo.com>
2024-06-25 22:31:09 +0530

    first
    second
    
    newcommitmessage#

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ function isValidVoter(birthYear) {
   const age = 2024 - birthYear;
   console.log(age > 21 ? "Eligible to vote" : "Not eligible to vote");
 }
-function canDrink(birthYear){
-    const age = 2024 - birthYear;
-    console.log(age > 25 ? "Eligible to drink" : "Not eligible to drink");
-}
+
 getAge(2002);
 isValidVoter(2000);
-canDrink(2001);
+
 

46f3d675f21631c153c3cd0849291201df505554
tusharrgoel <tushar.goel@practo.com>
2024-06-25 22:07:55 +0530

    feature1 commit

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ function isValidVoter(birthYear) {
   const age = 2024 - birthYear;
   console.log(age > 21 ? "Eligible to vote" : "Not eligible to vote");
 }
-function isLeapYear(year) {
-  console.log(year % 4 === '0' ? "Leap Year" : "Not a leap year");
-}
+
 getAge(2002);
 isValidVoter(2000);
-isLeapYear(2001);
+
